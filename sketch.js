4165f692929385e8e556ab829dcb88b3efcaf0c4
riteshtajrajchavhan <74711100+riteshtajrajchavhan@users.noreply.github.com>
2021-03-06 18:33:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+
+function preload(){
+backImg=loadImage("back.gif");
+
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  
+
+
+  back=createSprite(windowWidth/2,windowHeight/2);
+  back.addImage(backImg);
+  back.scale=2.5;
+
+  
+
+//createing  the road
+
+
+}
+
+function draw() { 
+  background(0);
+  drawSprites();
+ 
+  
+
+
+}
+
+
+
